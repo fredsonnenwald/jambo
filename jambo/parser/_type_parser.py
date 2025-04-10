eb9427c72e3aa1e446702943a12e008791b7b1f9
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2025-04-09 23:19:29 -0300

    Fixes Support for Python3.10

--- jambo/parser/_type_parser.py
+++ jambo/parser/_type_parser.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Generic, Self, TypeVar
+from typing import Generic, TypeVar
+from typing_extensions import Self
 
 from pydantic import Field
 
